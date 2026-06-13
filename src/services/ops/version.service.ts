519c52a2cbb7a5f549307e21db8b3a299fdfb07b
Michal Sierpien <sierpien.michal@gmail.com>
2026-06-13 09:43:43 +0200

    Bump API version to 1.1.1

--- src/services/ops/version.service.ts
+++ src/services/ops/version.service.ts
@@ -1,4 +1,4 @@
-export const API_VERSION = process.env.APP_VERSION || '1.1.0';
+export const API_VERSION = process.env.APP_VERSION || '1.1.1';
 export const API_CONTRACT_VERSION = Number(process.env.API_CONTRACT_VERSION || 2);
 export const MIN_ADMIN_CONTRACT_VERSION = Number(process.env.MIN_ADMIN_CONTRACT_VERSION || 2);
 export const MIN_ADMIN_VERSION = process.env.MIN_ADMIN_VERSION || '0.2.0';
